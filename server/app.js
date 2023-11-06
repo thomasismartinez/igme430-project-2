9882b6cbe8141ef73af974d4dd8147b07d47c648
Thomas Martinez <tjm7126@rit.edu>
2023-11-06 10:13:23 -0500

    sasd

--- server/app.js
+++ server/app.js
@@ -13,10 +13,10 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const dbURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1/DomoMaker';
 mongoose.connect(dbURI).catch((err) => {
-    if(err) {
-        console.log('Could not connect to database');
-        throw err;
-    }
+  if (err) {
+    console.log('Could not connect to database');
+    throw err;
+  }
 });
 
 // part 2
@@ -26,15 +26,15 @@ app.use(helmet());
 app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
 app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
 app.use(compression());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.engine('handlebars', expressHandlebars.engine({ defaultLayout: ''}));
+app.engine('handlebars', expressHandlebars.engine({ defaultLayout: '' }));
 app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views`);
 
 router(app);
 
 app.listen(port, (err) => {
-    if (err) { throw err };
-    console.log(`Listening on port ${port}`);
+  if (err) { throw err; }
+  console.log(`Listening on port ${port}`);
 });
--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -1,74 +1,69 @@
 const models = require('../models');
-const Account = models.Account;
 
-const loginPage = (req, res) => {
-    return res.render('login');
-};
+const { Account } = models;
 
-const signupPage = (req, res) => {
-    return res.render('signup');
-};
+const loginPage = (req, res) => res.render('login');
 
-const logout = (req, res) => {
-    return res.redirect('/');
-};
+const signupPage = (req, res) => res.render('signup');
+
+const logout = (req, res) => res.redirect('/');
 
 const login = (req, res) => {
-    const username = `${req.body.username}`;
-    const pass = `${req.body.pass}`;
+  const username = `${req.body.username}`;
+  const pass = `${req.body.pass}`;
 
-    // ensure all fields are filled
-    if (!username || !pass) {
-        return res.status(400).json({error: 'All fields are required!'});
+  // ensure all fields are filled
+  if (!username || !pass) {
+    return res.status(400).json({ error: 'All fields are required!' });
+  }
+
+  return Account.authenticate(username, pass, (err, account) => {
+    // if error or account doesnt exist
+    if (err || !account) {
+      return res.status(401).json({ error: 'Wrong username and password!' });
     }
 
-    return Account.authenticate(username, pass, (err, account) => {
-        // if error or account doesnt exist
-        if (err || !account) {
-            return res.status(401).json({ error: 'Wrong username and password!' });
-        }
-
-        // otherwise take user to website
-        return res.json({ redirect: '/maker'});
-    });
+    // otherwise take user to website
+    return res.json({ redirect: '/maker' });
+  });
 };
 
 const signup = async (req, res) => {
-    const username = `${req.body.username}`;
-    const pass = `${req.body.pass}`;
-    const pass2 = `${req.body.pass2}`;
+  const username = `${req.body.username}`;
+  const pass = `${req.body.pass}`;
+  const pass2 = `${req.body.pass2}`;
 
-    // ensure all fields are filled
-    if (!username || !pass || !pass2) {
-        return res.status(400).json({error: 'All fields are required!'});
-    }
+  // ensure all fields are filled
+  if (!username || !pass || !pass2) {
+    return res.status(400).json({ error: 'All fields are required!' });
+  }
 
-    // esnure passwords match
-    if (pass !== pass2) {
-        return res.status(400).json({ error: 'Passwords do not match!'});
-    }
+  // esnure passwords match
+  if (pass !== pass2) {
+    return res.status(400).json({ error: 'Passwords do not match!' });
+  }
 
-    // try to create account
-    try {
-        const hash = await Account.generateHash(pass);
-        const newAccount = new Account({username, password: hash});
-        await newAccount.save();
-        return res.json({ redirect: '/maker' });
-    } catch (err) {
-        console.log(err);
-        // if username taken
-        if(err.code === 11000) {
-            return res.status(400).json({error: 'Username already in use!'});
-        }
-        // server side error
-        return res.status(500).json({ error: 'An error occured!'});
+  // try to create account
+  try {
+    const hash = await Account.generateHash(pass);
+    const newAccount = new Account({ username, password: hash });
+    await newAccount.save();
+    return res.json({ redirect: '/maker' });
+  } catch (err) {
+    console.log(err);
+    // if username taken
+    if (err.code === 11000) {
+      return res.status(400).json({ error: 'Username already in use!' });
     }
+    // server side error
+    return res.status(500).json({ error: 'An error occured!' });
+  }
 };
 
 module.exports = {
-    loginPage,
-    signupPage,
-    login,
-    logout,
-    signup,
-}
+  loginPage,
+  signupPage,
+  login,
+  logout,
+  signup,
+};
