733661576741957a45b1d344f753250145b58113
Thomas Martinez <tjm7126@rit.edu>
2023-11-05 22:13:28 -0500

    login and signup

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -10,15 +10,59 @@ const signupPage = (req, res) => {
 };
 
 const logout = (req, res) => {
-    return res.render('logout');
+    return res.redirect('/');
 };
 
 const login = (req, res) => {
-    
+    const username = `${req.body.username}`;
+    const pass = `${req.body.pass}`;
+
+    // ensure all fields are filled
+    if (!username || !pass) {
+        return res.status(400).json({error: 'All fields are required!'});
+    }
+
+    return Account.authenticate(username, pass, (err, account) => {
+        // if error or account doesnt exist
+        if (err || !account) {
+            return res.status(401).json({ error: 'Wrong username and password!' });
+        }
+
+        // otherwise take user to website
+        return res.json({ redirect: '/maker'});
+    });
 };
 
-const signup = (req, res) => {
-    
+const signup = async (req, res) => {
+    const username = `${req.body.username}`;
+    const pass = `${req.body.pass}`;
+    const pass2 = `${req.body.pass2}`;
+
+    // ensure all fields are filled
+    if (!username || !pass || !pass2) {
+        return res.status(400).json({error: 'All fields are required!'});
+    }
+
+    // esnure passwords match
+    if (pass !== pass2) {
+        return res.status(400).json({ error: 'Passwords do not match!'});
+    }
+
+    // try to create account
+    try {
+        const hash = await Account.generateHash(pass);
+        const newAccount = new Account({username, password: hash});
+        await newAccount.save();
+        return res.json({ redirect: '/maker' });
+    } catch (err) {
+        console.log(err);
+        // if username taken
+        if(err.code === 11000) {
+            return res.status(400).json({error: 'Username already in use!'});
+        }
+        // server side error
+        return res.status(500).json({ error: 'An error occured!'});
+    }
 };
 
 module.exports = {
--- server/router.js
+++ server/router.js
@@ -7,7 +7,7 @@ const router = (app) => {
     app.get('/signup', controllers.Account.signupPage);
     app.post('/signup', controllers.Account.signup);
 
-    app.get('/logout', controllers.Account.login);
+    app.get('/logout', controllers.Account.logout);
     app.get('/maker', controllers.Domo.makerPage);
     app.get('/', controllers.Account.loginPage);
 };
