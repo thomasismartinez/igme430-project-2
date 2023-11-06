18823e12f19f9b8bf7e0a5821640a13d6c26245f
Thomas Martinez <tjm7126@rit.edu>
2023-11-06 18:23:14 -0500

    redis setup

--- server/app.js
+++ server/app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const path = require('path');
 const express = require('express');
 const compression = require('compression');
@@ -7,11 +9,14 @@ const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
 const helmet = require('helmet');
 const session = require('express-session');
+const RedisStore = require('connect-redis').default;
+const redis = require('redis');
 
 const router = require('./router.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
+// connect mongoose
 const dbURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1/DomoMaker';
 mongoose.connect(dbURI).catch((err) => {
   if (err) {
@@ -20,33 +25,45 @@ mongoose.connect(dbURI).catch((err) => {
   }
 });
 
-// part 2
-const app = express();
+// connect redis
+const redisClient = redis.createClient({
+  url: process.env.REDISCLOUD_URL,
+});
 
-app.use(helmet());
-app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
-app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
-app.use(compression());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+redisClient.on('error', (err) => console.log('Redic Client Error', err));
 
-app.use(session({
-  key: 'sessionid',
-  // hashing seed
-  secret: 'Domo Arigato',
-  // only send to database if data changes
-  resave: false,
-  // wont save uninitialized sessions
-  saveUninitialized: false,
-}));
+redisClient.connect().then(() => {
+  const app = express();
 
-app.engine('handlebars', expressHandlebars.engine({ defaultLayout: '' }));
-app.set('view engine', 'handlebars');
-app.set('views', `${__dirname}/../views`);
+  app.use(helmet());
+  app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
+  app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
+  app.use(compression());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
 
-router(app);
+  app.use(session({
+    key: 'sessionid',
+    // store redis info
+    store: new RedisStore({
+      client: redisClient,
+    }),
+    // hashing seed
+    secret: 'Domo Arigato',
+    // only send to database if data changes
+    resave: false,
+    // wont save uninitialized sessions
+    saveUninitialized: false,
+  }));
 
-app.listen(port, (err) => {
-  if (err) { throw err; }
-  console.log(`Listening on port ${port}`);
+  app.engine('handlebars', expressHandlebars.engine({ defaultLayout: '' }));
+  app.set('view engine', 'handlebars');
+  app.set('views', `${__dirname}/../views`);
+
+  router(app);
+
+  app.listen(port, (err) => {
+    if (err) { throw err; }
+    console.log(`Listening on port ${port}`);
+  });
 });
