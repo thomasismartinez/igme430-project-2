bba9aa04bac464485bad18ad8d56e97d847676f4
Thomas Martinez <tjm7126@rit.edu>
2023-11-06 19:04:17 -0500

    middleware

--- server/router.js
+++ server/router.js
@@ -1,18 +1,21 @@
 const controllers = require('./controllers');
+const mid = require('./middleware');
 
 const router = (app) => {
-  app.get('/login', controllers.Account.loginPage);
-  app.post('/login', controllers.Account.login);
+  console.log('entering jouter.js > router(app)');
+  app.get('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
+  console.log('survived line 5');
+  app.post('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.login);
 
-  app.get('/signup', controllers.Account.signupPage);
-  app.post('/signup', controllers.Account.signup);
+  app.get('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signupPage);
+  app.post('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signup);
 
-  app.get('/logout', controllers.Account.logout);
+  app.get('/logout', mid.requiresLogin, controllers.Account.logout);
 
-  app.get('/maker', controllers.Domo.makerPage);
-  app.post('/maker', controllers.Domo.makeDomo);
+  app.get('/maker', mid.requiresLogin, controllers.Domo.makerPage);
+  app.post('/maker', mid.requiresLogin, controllers.Domo.makeDomo);
 
-  app.get('/', controllers.Account.loginPage);
+  app.get('/', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
 };
 
 module.exports = router;
